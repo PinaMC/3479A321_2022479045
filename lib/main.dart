e0f5273089cbca48a3de030a781757f9f4a0377c
PinaMC <PinaMC@users.noreply.github.com>
2025-09-15 20:07:18 -0300

    fin lab, revisado y funcional

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,22 @@ import 'pages/my_home_page.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     var logger = Logger();
-    logger.d("Logger is working! in build method of MyApp"); // se supone que sale al inicio
+    logger.d("Logger is working!"); // se supone que sale al inicio
 
     return MaterialApp(
-
       title: '2022479045',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        textTheme: GoogleFonts.latoTextTheme( 
-          Theme.of(context).textTheme,
-        ),
+        textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme,),
         useMaterial3: true, //es como un estilo visual nuevo de google
       ),
       home: const MyHomePage(title: '2022479045'),
-    );
+    );  
   }
 }
